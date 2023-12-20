3a010d7cc880df60f14e4eb1f0edb7194d3d569c
ParkGyuMyeong <pkm200000@naver.com>
2023-12-20 18:37:18 +0900

    잠시푸시

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/resource/gyu/Calendar.dart';
+import 'package:myapp/resource/gyu/CalorieScore.dart';
 import 'package:myapp/resource/kong/first_page.dart';
 import 'package:myapp/utils.dart';
-import 'package:myapp/resource/gyu/calendarDetails_usageHistory.dart';
+import 'package:myapp/resource/gyu/calendar_Details.dart';
 import 'firebase_options.dart';
 
 void main() async {
