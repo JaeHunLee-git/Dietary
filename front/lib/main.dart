e97b15ff01a5dfa981d12376637bd1a0db1c4c44
ParkGyuMyeong <pkm200000@naver.com>
2023-12-22 09:08:11 +0900

    12.21work2

--- front/lib/main.dart
+++ front/lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         body: SingleChildScrollView(
-          child: Calendar(),
+          child: mainpage(),
         ),
       ),
     );
